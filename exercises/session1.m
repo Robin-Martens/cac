81f2c03254e001e488d4386ecd508ea70ab43bdb
Robin Martens <r0885874@pc-klas2-16.esat.kuleuven.be>
2025-02-20 17:57:27 +0100

    feat: more progress on session 1

--- exercises/session1.m
+++ exercises/session1.m
@@ -34,9 +34,48 @@ print_inverse(~z3);
 
 print_inverse_lagrange := procedure(~z)
 	o := Order(ZN ! z);
-	o;
+	if o ne 0 then
+		printf "The inverse of %o is: %o.\n", z, z^(o-1);
+	else
+		printf "No inverse was found for %o, as the order is zero.\n", z;
+	end if;
 end procedure;
+
+printf "The Euler Phi function of 105 is:, %o.\n", EulerPhi(105);
 
 print_inverse_lagrange(~z1);
 print_inverse_lagrange(~z2);
 print_inverse_lagrange(~z3);
+
+////////////////
+// Exercise 2 //
+////////////////
+
+F31 := GF(31);
+
+for x in F31 do
+	res := Modexp(Z ! x, 31, 31);
+	if res eq x then
+		printf "[SUCCESS]: Fermat's Little Theorem holds for %o.\n", x;
+	else
+		printf "[FAILURE]: Fermat's Little Theorem does not hold for %o.\n", x;
+	end if;
+end for;
+
+////////////////
+// Exercise 3 //
+////////////////
+
+R := PolynomialRing(F31);
+R.1;
+
+GenRandomIrreduciblePol := function(K, n)
+	f := Random(K);
+	while not IsInvertible(f) do
+	    f := Random(K);
+	end while;
+	return f;
+end function;
+
+GenRandomIrreduciblePol(R, 5);
+
