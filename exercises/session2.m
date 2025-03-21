5135904939fa869b0d0db2ccc71d6dc9d1ec36e3
Robin Martens <robin.martens1@student.kuleuven.be>
2025-03-21 17:38:01 +0100

    feat: finish session 2

--- exercises/session2.m
+++ exercises/session2.m
@@ -31,7 +31,91 @@ function RSADecryptCRT(c, N, d, e)
 
 end function;
 
-N, e, d := RSAKeyGen(4096);
-c := RSAEncrypt(123, N, e);
-print RSADecrypt(c, N, d);
+// N, e, d := RSAKeyGen(4096);
+// c := RSAEncrypt(123, N, e);
+// print RSADecrypt(c, N, d);
 
+////////////////
+// Exercise 2 //
+////////////////
+
+function SquareMult(a, n)
+  if a eq 0 then
+    if n lt 0 then error "Negative power of non-invertible element"; end if;
+    return Parent(a) ! 0;
+  end if;
+
+  if n eq 0 then return Parent(a) ! 1; end if;
+  if n lt 0 then return SquareMult(1/a, -n); end if;
+
+  powa := a;
+  res := Parent(a) ! 1;
+  e := n;
+
+  while e gt 0 do
+    if (e mod 2) eq 1 then res := res * powa; end if;
+    powa := powa^2;
+    e := e div 2;
+  end while;
+
+  return res;
+end function;
+
+F := GF(NextPrime(2^1000 : Proof := false));
+
+// t := Cputime();
+// for i := 1 to 1000 do
+//   A := Random(F)^Random(2^1000);
+// end for;
+// Cputime(t);
+// t := Cputime();
+// for i := 1 to 1000 do
+//   A := SquareMult(Random(F),Random(2^1000));
+// end for;
+// Cputime(t);
+
+////////////////
+// Exercise 3 //
+////////////////
+
+function StringToHill(s)
+  if not Regexp("^[A-Z]+$", s) then
+    error "The input string should only contain capital letters!";
+  else
+    return [Integers(26) ! StringToCode(s[i]) - 65 : i in [1..#s]];
+  end if;
+end function;
+
+function HillToString(a)
+  return &cat[CodeToString(((Integers() ! a[i]) + 65)) : i in [1..#a]];
+end function;
+
+function HillKeyGen(s)
+  if not IsSquare(#s) then
+    error "The length of the string should be square!";
+  end if;
+
+  k := Integers() ! Sqrt(#s);
+  mat := ZeroMatrix(Integers(26), k, k);
+  hill := StringToHill(s);
+
+  i := 0;
+  for i in [1..#hill] do
+    mat[Ceiling(i / k)][((i - 1) mod k) + 1] := hill[i];
+  end for;
+
+  return mat;
+end function;
+
+function HillEncrypt(s, A)
+  hill := StringToHill(s);
+  k := Nrows(A);
+  padLength := #hill mod k;
+end function;
+
+a :=  StringToHill("TEST");
+print a;
+
+print HillToString(a);
+print HillKeyGen("ABXZ");
+print HillKeyGen("ABXZ");
